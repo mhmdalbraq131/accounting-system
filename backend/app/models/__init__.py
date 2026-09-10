2317987049392ba755c4451f40c93b0d51e952cc
mhmdalbraq131 <mhmdalbraq131@gmail.com>
2026-09-10 14:56:29 +0300

    chore: register domain models

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,6 @@
+from app.models.account import Account
+from app.models.financial import FinancialAccount
+from app.models.journal import JournalEntry, JournalLine
+from app.models.party import Party
+
+__all__ = ["Account", "FinancialAccount", "JournalEntry", "JournalLine", "Party"]
