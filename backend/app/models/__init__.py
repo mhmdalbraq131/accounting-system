1adb2b13f6290061ef9dc0abd581e368f0070ab5
mhmdalbraq131 <mhmdalbraq131@gmail.com>
2026-09-10 15:07:06 +0300

    feat: register accounting domain models

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,5 +2,11 @@ from app.models.account import Account
 from app.models.financial import FinancialAccount
 from app.models.journal import JournalEntry, JournalLine
 from app.models.party import Party
+from app.models.role import Permission, Role, RolePermission, UserRole
+from app.models.user import User
+from app.models.voucher import Voucher
 
-__all__ = ["Account", "FinancialAccount", "JournalEntry", "JournalLine", "Party"]
+__all__ = [
+    "Account", "FinancialAccount", "JournalEntry", "JournalLine", "Party",
+    "Permission", "Role", "RolePermission", "UserRole", "User", "Voucher",
+]
