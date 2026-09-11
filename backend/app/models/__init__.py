c0fbaa335445e7672119bf2d425686aed7a68fea
mhmdalbraq131 <mhmdalbraq131@gmail.com>
2026-09-11 19:16:47 +0300

    feat: register branch model

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,4 +1,5 @@
 from app.models.account import Account
+from app.models.branch import Branch
 from app.models.expense import Expense
 from app.models.financial import FinancialAccount
 from app.models.journal import JournalEntry, JournalLine
@@ -10,7 +11,7 @@ from app.models.user import User
 from app.models.voucher import Voucher
 
 __all__ = [
-    "Account", "Expense", "FinancialAccount", "JournalEntry", "JournalLine", "Party",
+    "Account", "Branch", "Expense", "FinancialAccount", "JournalEntry", "JournalLine", "Party",
     "Permission", "Role", "RolePermission", "UserRole", "User", "Voucher", "SystemSetting",
     "TravelProgram", "Pilgrim", "ProgramBooking", "VisaService",
 ]
